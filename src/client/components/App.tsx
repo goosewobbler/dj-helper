ec0822edc7f75ed322d1dac178a84792d386c35f
Sam Maister <sam.maister@bbc.co.uk>
2019-07-27 14:19:00 +0100

    revert comments now that glamorous has been killed

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,27 +1,34 @@
 import * as React from 'react';
 
-// import ComponentDetailsContainer from '../containers/ComponentDetailsContainer';
-// import ComponentListContainer from '../containers/ComponentListContainer';
-// import ComponentListFilterContainer from '../containers/ComponentListFilterContainer';
-// import UpdateBar from '../containers/UpdateBarContainer';
-// import CreateForm from './CreateForm';
-// import Dialog from './Dialog';
+import ComponentDetailsContainer from '../containers/ComponentDetailsContainer';
+import ComponentListContainer from '../containers/ComponentListContainer';
+import ComponentListFilterContainer from '../containers/ComponentListFilterContainer';
+import UpdateBar from '../containers/UpdateBarContainer';
+import CreateForm from './CreateForm';
+import Dialog from './Dialog';
 import ExternalLink from './ExternalLink';
 import GitHubLink from './GitHubLink';
-// import LabelButton from './LabelButton';
+import LabelButton from './LabelButton';
 
 interface IAppProps {
   shouldShowCreateDialog: boolean;
-  onCreate(): any;
+//  onCreate(): any;
   showCreateDialog(show: boolean): any;
   submitModule(name: string, description: string, type: string): any;
 }
 
 const App = (props: IAppProps) => (
   <div className="container">
+    <UpdateBar />
     <div className="header">
       <h1 className="title">Morph Developer Console</h1>
       <div className="header-links">
+        <LabelButton
+          className="create-button"
+          label="Create"
+          image="/image/icon/gel-icon-add.svg"
+          onClick={() => props.showCreateDialog(true)}
+        />
         <ExternalLink
           link="https://ci.sport.tools.bbc.co.uk/view/morph-module-pipeline/"
           label="INT Pipeline"
@@ -41,7 +48,18 @@ const App = (props: IAppProps) => (
       </div>
     </div>
     <div className="content">
-      Weeeeeeeeeeeeee
+      <div className="section">
+        <ComponentListFilterContainer />
+        <ComponentListContainer />
+      </div>
+      <div className="section">
+        <ComponentDetailsContainer />
+      </div>
+      {props.shouldShowCreateDialog ? (
+        <Dialog title="Create a new Morph module" onClose={() => props.showCreateDialog(false)}>
+          <CreateForm submitModule={props.submitModule} onClose={() => props.showCreateDialog(false)} />
+        </Dialog>
+      ) : null}
     </div>
   </div>
 );

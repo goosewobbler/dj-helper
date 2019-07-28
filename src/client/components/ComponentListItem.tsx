9b0be517a286fc6a9d508c3c98dcf88845eea5a8
Sam Maister <sam.maister@bbc.co.uk>
2019-07-28 22:01:14 +0100

    update for newer stuff

--- src/client/components/ComponentListItem.tsx
+++ src/client/components/ComponentListItem.tsx
@@ -2,27 +2,14 @@ import * as React from 'react';
 
 import ComponentState from '../../types/ComponentState';
 import ComponentData from '../../types/ComponentData';
-import ExternalLink from './ExternalLink';
+import ExternalLink from '../ui/ExternalLink';
+import LoadingIcon from '../ui/icon/LoadingIcon';
+import PauseIcon from '../ui/icon/PauseIcon';
+import PlayIcon from '../ui/icon/PlayIcon';
+import StarIcon from '../ui/icon/StarIcon';
 import IconButton from './IconButton';
 
 const createID = (componentName: string) => `component-list-item-${componentName}`;
-
-const backgroundColorForState = (state: ComponentState) => {
-  switch (state) {
-    case ComponentState.Linking:
-    case ComponentState.Starting:
-      return 'orange';
-    case ComponentState.Installing:
-      return 'rgb(255, 90, 90)';
-    case ComponentState.Building:
-      return '#cb75ff';
-    case ComponentState.Running:
-      return '#baecff';
-    default:
-      break;
-  }
-  return 'white';
-};
 
 interface IComponentListItemProps {
   component: ComponentData;
@@ -36,21 +23,15 @@ interface IComponentListItemProps {
 const renderFavouriteButton = (props: IComponentListItemProps) => {
   if (props.component.favorite) {
     return (
-      <IconButton
-        className="unfavorite-button"
-        label="Unfavorite"
-        image="/image/icon/favourited.svg"
-        onClick={() => props.onToggleFavourite()}
-      />
+      <IconButton className="unfavorite-button" label="Unfavorite" onClick={() => props.onFavourite(props.name, false)}>
+        <StarIcon starred />
+      </IconButton>
     );
   }
   return (
-    <IconButton
-      className="favorite-button"
-      label="Favorite"
-      image="/image/icon/unfavourited.svg"
-      onClick={() => props.onToggleFavourite()}
-    />
+    <IconButton className="favorite-button" label="Favorite" onClick={() => props.onFavourite(props.name, true)}>
+      <StarIcon starred={false} />
+    </IconButton>
   );
 };
 
@@ -67,60 +48,92 @@ const renderLaunchButton = (component: ComponentData) => {
   return null;
 };
 
-const renderStartStopButton = (props: IComponentListItemProps) => {
-  switch (props.component.state) {
+const renderStartStopButton = (props: IComponentListItemProps, handleStart: () => void, handleStop: () => void) => {
+  switch (props.state) {
     case ComponentState.Stopped:
       return (
-        <IconButton
-          className="start-button"
-          label="Start"
-          image="/image/icon/gel-icon-play.svg"
-          onClick={props.onStart}
-        />
+        <IconButton className="start-button" label="Start" onClick={handleStart}>
+          <PlayIcon />
+        </IconButton>
       );
     case ComponentState.Installing:
-      return <IconButton label="Installing" loading />;
+      return (
+        <IconButton label="Installing">
+          <LoadingIcon />
+        </IconButton>
+      );
     case ComponentState.Building:
-      return <IconButton label="Building" loading />;
+      return (
+        <IconButton label="Building">
+          <LoadingIcon />
+        </IconButton>
+      );
     case ComponentState.Linking:
-      return <IconButton label="Linking" loading />;
+      return (
+        <IconButton label="Linking">
+          <LoadingIcon />
+        </IconButton>
+      );
     case ComponentState.Starting:
-      return <IconButton label="Starting" loading />;
+      return (
+        <IconButton label="Starting">
+          <LoadingIcon />
+        </IconButton>
+      );
     case ComponentState.Running:
       return (
-        <IconButton
-          className="stop-button"
-          label="Stop"
-          image="/image/icon/gel-icon-pause.svg"
-          onClick={props.onStop}
-        />
+        <IconButton className="stop-button" label="Stop" onClick={handleStop}>
+          <PauseIcon />
+        </IconButton>
       );
   }
 };
 
 class ComponentListItem extends React.PureComponent<IComponentListItemProps> {
-  public shouldComponentUpdate(nextProps: IComponentListItemProps) {
-    if (this.props.component !== nextProps.component) {
-      return true;
-    }
-    if (this.props.selected !== nextProps.selected) {
+  public static displayName = 'ComponentListItem';
+
+  private handleClick: () => void;
+  private handleStart: () => void;
+  private handleStop: () => void;
+
+  constructor(props: IComponentListItemProps) {
+    super(props);
+    this.handleClick = () => this.props.onClick(this.props.name);
+    this.handleStart = () => this.props.onStart(this.props.name);
+    this.handleStop = () => this.props.onStop(this.props.name);
+  }
+
+  public shouldComponentUpdate(nextProps: IComponentListItemProps, nextState: {}, context: any) {
+    if (
+      this.props.name !== nextProps.name ||
+      this.props.displayName !== nextProps.displayName ||
+      this.props.highlighted !== nextProps.highlighted ||
+      this.props.url !== nextProps.url ||
+      this.props.favourite !== nextProps.favourite ||
+      this.props.state !== nextProps.state ||
+      this.props.selected !== nextProps.selected
+    ) {
       return true;
     }
     return false;
   }
 
   public render() {
-    const name: string =
-      this.props.component.highlighted && this.props.component.highlighted.length > 0
-        ? this.props.component.highlighted
-        : this.props.component.displayName;
+    const name: any =
+      this.props.highlighted && this.props.highlighted.length > 0 ? this.props.highlighted : this.props.displayName;
 
     return (
-      <div role="button" id={createID(this.props.component.name)}>
+      <div
+        theme={this.props.theme}
+        role="button"
+        id={createID(this.props.name)}
+        highlighted={this.props.selected}
+        onClick={this.handleClick}
+      >
         {renderFavouriteButton(this.props)}
         <span className="component-name-label">{name}</span>
         {renderLaunchButton(this.props.component)}
-        {renderStartStopButton(this.props)}
+        {renderStartStopButton(this.props, this.handleStart, this.handleStop)}
       </div>
     );
   }

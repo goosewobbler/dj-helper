2ee85ef2296eddd14aeca0dcd3379bb968c5bcaa
Sam Maister <sam.maister@bbc.co.uk>
2019-07-28 21:20:42 +0100

    remove glsmorous, recompose, theming

--- src/client/ui/ExternalLink.tsx
+++ src/client/ui/ExternalLink.tsx
@@ -1,37 +1,8 @@
-import * as Color from 'color';
-import glamorous from 'glamorous';
 import * as React from 'react';
-import { pure } from 'recompose';
 
-import ITheme from '../../types/ITheme';
 import ExternalIcon from './icon/ExternalIcon';
 
-const A = glamorous.a(
-  {
-    borderRadius: '4px',
-    color: 'black',
-    flexShrink: 0,
-    fontSize: '16px',
-    textDecoration: 'none',
-  },
-  (props: { theme: ITheme; black: boolean; color: string; height: string; padding: string }) => ({
-    backgroundColor: props.color,
-    border: `1px solid ${Color(props.theme.primaryTextColour).fade(0.7)}`,
-    color: props.black ? props.theme.primaryTextColour : props.theme.secondaryTextColour,
-    height: props.height || 'auto',
-    padding: props.padding || '8px',
-  }),
-);
-
-const ImageContainerDiv = glamorous.div({
-  display: 'inline-flex',
-  height: '12px',
-  margin: '0 8px 0 4px',
-  width: '12px',
-});
-
 interface IExternalLinkProps {
-  theme: ITheme;
   black?: boolean;
   className?: string;
   color?: string;
@@ -42,22 +13,12 @@ interface IExternalLinkProps {
 }
 
 const ExternalLink = (props: IExternalLinkProps) => (
-  <A
-    theme={props.theme}
-    className={props.className}
-    href={props.link}
-    target="_blank"
-    color={props.color}
-    black={props.black}
-    padding={props.padding}
-    height={props.height}
-    onClick={event => event.stopPropagation()}
-  >
-    <ImageContainerDiv>
-      <ExternalIcon colour={props.black ? props.theme.primaryTextColour : props.theme.secondaryTextColour} />
-    </ImageContainerDiv>
+  <a className={props.className} href={props.link} target="_blank" onClick={event => event.stopPropagation()}>
+    <div className="container">
+      <ExternalIcon />
+    </div>
     <span>{props.label}</span>
-  </A>
+  </a>
 );
 
-export default pure(ExternalLink);
+export default ExternalLink;

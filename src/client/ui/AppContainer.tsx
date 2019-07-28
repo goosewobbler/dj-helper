befb1f1c21c498ae5a240b487e72b706e88d4633
Sam Maister <sam.maister@bbc.co.uk>
2019-07-28 21:16:13 +0100

    remove glamorous, recompose, theming

--- src/client/ui/AppContainer.tsx
+++ src/client/ui/AppContainer.tsx
@@ -1,58 +1,4 @@
-import glamorous from 'glamorous';
 import * as React from 'react';
-import { pure } from 'recompose';
-
-import ITheme from '../../types/ITheme';
-import Spacer from './Spacer';
-
-const LIST_PROPORTION = 0.3;
-
-const ContainingDiv = glamorous.div(
-  {
-    display: 'flex',
-    flexDirection: 'column',
-    flexGrow: 1,
-  },
-  (props: { theme: ITheme }) => ({
-    backgroundColor: props.theme.primaryBackgroundColour,
-    fontFamily: props.theme.font,
-  }),
-);
-
-const HeaderDiv = glamorous.div(
-  {
-    alignItems: 'center',
-    borderBottom: '1px solid rgba(0, 0, 0, 0.075)',
-    boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.05)',
-    display: 'flex',
-    flexShrink: 0,
-    justifyContent: 'space-between',
-    padding: '12px',
-    zIndex: 1,
-  },
-  (props: { theme: ITheme }) => ({
-    backgroundColor: props.theme.headerColour,
-  }),
-);
-
-const ContentDiv = glamorous.div({
-  display: 'flex',
-  flexGrow: 1,
-});
-
-const SectionDiv = glamorous.div(
-  {
-    display: 'flex',
-    flexDirection: 'column',
-    flexGrow: 0,
-    flexShrink: 0,
-    overflow: 'hidden',
-    padding: '8px',
-  },
-  (props: { proportion: number }) => ({
-    flexBasis: `${props.proportion * 100}%`,
-  }),
-);
 
 interface IAppContainerProps {
   banner: any;
@@ -60,20 +6,18 @@ interface IAppContainerProps {
   header: any;
   leftPanel: any;
   rightPanel: any;
-  theme: ITheme;
 }
 
 const AppContainer = (props: IAppContainerProps) => (
-  <ContainingDiv theme={props.theme}>
+  <div>
     {props.banner}
-    <HeaderDiv theme={props.theme}>{props.header}</HeaderDiv>
-    <Spacer />
-    <ContentDiv>
-      <SectionDiv proportion={LIST_PROPORTION}>{props.leftPanel}</SectionDiv>
-      <SectionDiv proportion={1 - LIST_PROPORTION}>{props.rightPanel}</SectionDiv>
+    <div className="header">{props.header}</div>
+    <div className="content">
+      <div className="section">{props.leftPanel}</div>
+      <div className="section">{props.rightPanel}</div>
       {props.dialog}
-    </ContentDiv>
-  </ContainingDiv>
+    </div>
+  </div>
 );
 
-export default pure(AppContainer);
+export default AppContainer;

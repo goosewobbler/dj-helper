a9d8bcb50b8acbfb1bfaa660183d15fef9b74215
Sam Maister <sam.maister@bbc.co.uk>
2019-09-01 03:17:39 +0100

    fix build tooling somewhat

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['react-hot-loader/patch', './src/indexClient.tsx'],
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+
+  plugins: [new webpack.NamedModulesPlugin(), new webpack.HotModuleReplacementPlugin()],
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      {
+        test: /\.tsx?$/,
+        loaders: ['react-hot-loader/webpack', 'awesome-typescript-loader'],
+        exclude: path.resolve(__dirname, 'node_modules'),
+        include: path.resolve(__dirname, 'src'),
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+
+  devServer: {
+    contentBase: './static',
+    hot: true,
+  },
+};

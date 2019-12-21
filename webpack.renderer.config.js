33836239f58fce2e1e16baad55b51d8fd6ced14e
Sam Maister <sam.maister@bbc.co.uk>
2019-12-21 20:31:06 +0000

    kinda working CSS & HTML build

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,15 +1,38 @@
 /* eslint global-require: off */
-
-// import tailwind from 'tailwindcss';
-// import autoprefixer from 'autoprefixer';
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const tailwind = require('tailwindcss');
+// const autoprefixer = require('autoprefixer');
 const rules = require('./webpack.rules');
+const plugins = require('./webpack.plugins');
+
+plugins.push(
+  new HtmlWebPackPlugin({
+    template: './public/index.html',
+    filename: './index.html',
+  }),
+);
 
 rules.push({
   test: /\.css$/,
+  exclude: /node_modules/,
   use: [
-    'style-loader',
-    { loader: 'css-loader', options: { importLoaders: 1 } },
-    'postcss-loader',
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: process.env.NODE_ENV === 'development',
+      },
+    },
+    {
+      loader: 'css-loader',
+      options: { importLoaders: 1, sourceMap: true },
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+      },
+    },
     // {
     //   loader: 'postcss-loader',
     //   options: {
@@ -21,14 +44,19 @@ rules.push({
 });
 
 module.exports = {
-  entry: './src/renderer/index.tsx',
+  mode: process.env.NODE_ENV,
+  entry: {
+    app: './src/renderer/index.tsx',
+    // styles: './src/renderer/css/tailwind.src.css',
+  },
   output: {
-    filename: './dist/renderer.prod.js',
+    path: `${__dirname}/dist`,
+    filename: '[name].prod.js',
   },
   module: {
     rules,
   },
-  plugins: require('./webpack.plugins'),
+  plugins,
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
   },

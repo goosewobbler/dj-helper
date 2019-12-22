c53868ba98388e823f2a76feadb78343e9beab22
Sam Maister <sam.maister@bbc.co.uk>
2019-12-22 16:37:51 +0000

    add more bits from electron-react-boilerplate

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,4 +1,6 @@
 /* eslint global-require: off */
+const path = require('path');
+const spawn = require('child_process').spawn;
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const tailwind = require('tailwindcss');
@@ -12,6 +14,9 @@ plugins.push(
     filename: './index.html',
   }),
 );
+
+const port = process.env.PORT || 1212;
+const publicPath = `http://localhost:${port}/dist`;
 
 rules.push({
   test: /\.css$/,
@@ -61,5 +66,38 @@ module.exports = {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
   },
   target: 'electron-renderer',
+  devServer: {
+    port,
+    publicPath,
+    compress: true,
+    noInfo: true,
+    stats: 'errors-only',
+    inline: true,
+    lazy: false,
+    hot: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    contentBase: path.join(__dirname, 'dist'),
+    watchOptions: {
+      aggregateTimeout: 300,
+      ignored: /node_modules/,
+      poll: 100,
+    },
+    historyApiFallback: {
+      verbose: true,
+      disableDotRule: false,
+    },
+    before() {
+      if (process.env.START_HOT) {
+        console.log('Starting Main Process...');
+        spawn('yarn', ['dev:start-main'], {
+          shell: true,
+          env: process.env,
+          stdio: 'inherit',
+        })
+          .on('close', code => process.exit(code))
+          .on('error', spawnError => console.error(spawnError));
+      }
+    },
+  },
   // externals: [],
 };

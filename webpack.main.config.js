41239924552ff7c1e08a557ec7d9c598388603f1
Sam Maister <sam.maister@bbc.co.uk>
2019-12-22 18:26:44 +0000

    add main dev stuff

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,17 +1,19 @@
 /* eslint global-require: off */
 const plugins = require('./webpack.main.plugins');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
   mode: process.env.NODE_ENV,
-  devtool: 'source-map',
+  devtool: isDev ? 'inline-source-map' : 'source-map',
   entry: './src/main/index.ts',
   output: {
     path: `${__dirname}/dist`,
-    filename: 'main.prod.js',
+    filename: `main.${isDev ? 'dev' : 'prod'}.js`,
   },
   module: {
     rules: require('./webpack.rules'),

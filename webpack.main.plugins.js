8fa2a20b662814d105c32eef1e715d29ac68b5e8
Sam Maister <sam.maister@bbc.co.uk>
2019-12-22 17:23:02 +0000

    remove clean-webpack-plugin

--- webpack.main.plugins.js
+++ webpack.main.plugins.js
@@ -1,12 +1,10 @@
 /* eslint global-require: off */
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const isDev = process.env.NODE_ENV === 'development';
 
 const plugins = [
-  new CleanWebpackPlugin(),
   new BundleAnalyzerPlugin({
     analyzerMode: 'static',
     generateStatsFile: true,

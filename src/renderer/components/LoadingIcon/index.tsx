847345d9ba363b9867514d2e9e50fd763453ff27
Sam Maister <sam.maister@bbc.co.uk>
2020-01-06 00:57:00 +0000

    add styles to LoadingIcon

--- src/renderer/components/LoadingIcon/index.tsx
+++ src/renderer/components/LoadingIcon/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 
+// TODO: https://github.com/benface/tailwindcss-animations
+// animation: 'spin 0.5s infinite linear',
+
 const LoadingIcon = ({ colour }: { colour?: string }): React.ReactElement => (
-  <div className="spinner">
+  <div className="spinner w-full h-full">
     <svg width="100%" height="100%" viewBox="0 0 32 32" fill={colour}>
       <path d="M29.8 8l-3.5 2c1 1.8 1.6 3.8 1.6 6 0 6.6-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4V0C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16c0-2.9-.8-5.6-2.2-8z" />
     </svg>

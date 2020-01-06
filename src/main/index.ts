ffc18e28e66075342330131280b46fd65313dc52
Sam Maister <sam.maister@bbc.co.uk>
2020-01-06 12:24:59 +0000

    clean up and add dev tools extensions

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,9 +6,10 @@ import { logError } from './helpers/console';
 if (module.hot) {
   module.hot.accept();
 }
+
 const isDev = process.env.NODE_ENV === 'development';
 
-let mainWindow: Electron.BrowserWindow;
+let mainWindow: BrowserWindow | undefined;
 
 if (process.env.NODE_ENV === 'production') {
   import('source-map-support').then(sourceMapSupport => sourceMapSupport.install());
@@ -18,11 +19,22 @@ if (isDev || process.env.DEBUG_PROD === 'true') {
   import('electron-debug').then(electronDebug => electronDebug.default());
 }
 
+const installExtensions = async (): Promise<string | void> => {
+  const installer = await import('electron-devtools-installer');
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = ['REACT_DEVELOPER_TOOLS', 'REACT_PERF', 'REDUX_DEVTOOLS'];
+
+  return installer.default(extensions, forceDownload).catch(logError);
+};
+
 async function createWindow(): Promise<void> {
+  if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+    await installExtensions();
+  }
   mainWindow = new BrowserWindow({
+    show: false,
     height: 800,
     width: 1200,
-    show: false,
     webPreferences: {
       nodeIntegration: true,
     },
@@ -35,43 +47,34 @@ async function createWindow(): Promise<void> {
   mainWindow.loadURL(`http://localhost:${apiPort}/`);
 
   mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-    mainWindow.webContents.openDevTools();
+    (mainWindow as BrowserWindow).show();
+    (mainWindow as BrowserWindow).webContents.openDevTools();
   });
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', (): void => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    // mainWindow = null;
+    mainWindow = undefined;
   });
-
-  // mainWindow.once('ready-to-show', () => {
-  //   mainWindow.show();
-  // });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
   createWindow();
 });
 
-// Quit when all windows are closed.
-app.on('window-all-closed', (): void => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+app.on('window-all-closed', () => {
+  // Respect the OSX convention of having the application in memory even
+  // after all windows have been closed
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', (): void => {
-  // On OS X it"s common to re-create a window in the app when the
+  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (mainWindow === undefined) {
     createWindow();
   }
 });

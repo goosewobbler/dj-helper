2237e9cf830f2467cae56195caf657769599e207
Sam Maister <sam.maister@bbc.co.uk>
2020-01-13 17:12:58 +0000

    move fillColour handling to the child icon

--- src/renderer/components/GithubLink/index.tsx
+++ src/renderer/components/GithubLink/index.tsx
@@ -1,18 +1,15 @@
 import { shell } from 'electron';
 import * as React from 'react';
-import tw from 'tailwind.macro';
-
 import GitHubIcon from './GithubIcon';
 
 const GitHubLink = ({ link }: { link: string }): React.ReactElement => {
-  const fillColor = tw`text-primary-text`.color;
   return (
     <button
-      className="w-8 h-8 m-1 mr-3 flex-shrink-0 text-hidden"
+      className="flex-shrink-0 w-8 h-8 m-1 mr-3 text-hidden"
       type="button"
       onClick={(): Promise<void> => shell.openExternal(link)}
     >
-      <GitHubIcon fillColor={fillColor} />
+      <GitHubIcon />
       View on GitHub
     </button>
   );

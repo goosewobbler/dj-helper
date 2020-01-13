293788ab3717f561e9a454bfd6e6f8969a3500a4
Sam Maister <sam.maister@bbc.co.uk>
2020-01-13 17:11:42 +0000

    standardise icons that don't change their colour

--- src/renderer/components/CreateIcon/index.tsx
+++ src/renderer/components/CreateIcon/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
+import tw from 'tailwind.macro';
 
-const CreateIcon = ({ colour }: { colour?: string }): React.ReactElement => (
-  <svg width="100%" height="100%" viewBox="0 0 32 32" fill={colour}>
-    <path d="M31.5 13.5h-13V.5h-5v13H.5v5h13v13h5v-13h13z" id="Layer_2" />
-  </svg>
-);
+const CreateIcon = (): React.ReactElement => {
+  const fillColor = tw`text-primary-text`.color;
+  return (
+    <svg width="100%" height="100%" viewBox="0 0 32 32" fill={fillColor}>
+      <path d="M31.5 13.5h-13V.5h-5v13H.5v5h13v13h5v-13h13z" id="Layer_2" />
+    </svg>
+  );
+};
 
 export default CreateIcon;

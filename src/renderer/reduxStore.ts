924d72df713d6ef8110c2ae839dfb307329d4c51
Sam Maister <sam.maister@bbc.co.uk>
2020-01-14 13:34:06 +0000

    use redux-devtools

--- src/renderer/reduxStore.ts
+++ src/renderer/reduxStore.ts
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore, Store } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import combinedReducer from './reducers/combined';
 import { AppState, Dispatch } from '../common/types';
 
@@ -10,11 +11,18 @@ const createDefaultState = (): AppState => ({
   },
 });
 
-const createReduxStore = (initialState?: AppState): Store =>
-  createStore(
+const createReduxStore = (initialState?: AppState): Store => {
+  const isDev = process.env.NODE_ENV !== 'production' || process.env.DEBUG_PROD;
+  const composeEnhancers = composeWithDevTools({
+    trace: true,
+  });
+  const storeEnhancer = applyMiddleware<Dispatch, AppState>(thunkMiddleware);
+
+  return createStore(
     combinedReducer,
     initialState || createDefaultState(),
-    applyMiddleware<Dispatch, AppState>(thunkMiddleware),
+    isDev ? composeEnhancers(storeEnhancer) : storeEnhancer,
   );
+};
 
 export default createReduxStore;

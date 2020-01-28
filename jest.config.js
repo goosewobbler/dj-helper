428c3e685c3813acd5d6da96409d5c1278faa51e
Sam Maister <sam.maister@bbc.co.uk>
2020-01-28 23:41:45 +0000

    squash ts-jest warning

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: {
-    '.(ts|tsx)': '<rootDir>/node_modules/ts-jest/preprocessor.js',
+    '.(ts|tsx)': 'ts-jest',
   },
   collectCoverageFrom: ['src/**/*'],
   coverageThreshold: {

cfd906289fd5681f5894ca8b7a1538167cf49a30
Sam Maister <sam.maister@bbc.co.uk>
2020-01-28 21:05:48 +0000

    use tailwind animations for spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,29 @@
 module.exports = {
   theme: {
     extend: {},
+    animations: {
+      spin: {
+        from: {
+          transform: 'rotate(0deg)',
+        },
+        to: {
+          transform: 'rotate(360deg)',
+        },
+      },
+    },
+    animationDuration: {
+      'default': '1s',
+      '0s': '0s',
+      '500ms': '0.5s',
+      '1s': '1s',
+      '2s': '2s',
+      '3s': '3s',
+      '4s': '4s',
+      '5s': '5s',
+    },
   },
   variants: {
     borderColor: ['responsive', 'hover', 'focus', 'focus-within', 'active'],
   },
-  plugins: [require('./tailwind.theme.config')],
+  plugins: [require('./tailwind.theme.config'), require('tailwindcss-animations')()],
 };

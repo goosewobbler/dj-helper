281aa36889414ff2de11d4916a386a0ee499eaca
Sam Maister <sam.maister@bbc.co.uk>
2020-02-19 12:59:22 +0000

    de-classify + use hooks

--- src/renderer/components/ComponentDetails/Tabs/index.tsx
+++ src/renderer/components/ComponentDetails/Tabs/index.tsx
@@ -7,60 +7,44 @@ interface TabsProps {
   buttons?: React.ReactNode;
 }
 
-interface TabsState {
-  selectedIndex: number;
-}
+const renderHeadings = (
+  headings: string[],
+  selectedIndex: number,
+  onClick: (index: number) => void,
+): React.ReactNode[] =>
+  headings.map(
+    (heading, index): React.ReactNode => {
+      const selected = selectedIndex === index;
+      const borderBottomWidth = `border-b-${selected ? '4' : '2'}`;
+      const borderBottomColor = `border-${selected ? 'selected-item' : 'primary-text'}`;
+      return (
+        <button
+          className={`p-1 outline-none border-0 text-lg my-0 mx-2 text-primary-text border-solid ${borderBottomWidth} ${borderBottomColor}`}
+          type="button"
+          key={heading}
+          onClick={(): void => onClick(index)}
+        >
+          {heading}
+        </button>
+      );
+    },
+  );
 
-class Tabs extends React.Component<TabsProps, TabsState> {
-  public constructor(props: TabsProps) {
-    super(props);
+const Tabs = ({ children, headings, headingChildren }: TabsProps): React.ReactElement => {
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const onClick = (index: number): void => {
+    setSelectedIndex(index);
+  };
 
-    this.state = {
-      selectedIndex: 0,
-    };
-  }
-
-  private renderHeadings(): React.ReactNode[] {
-    const { selectedIndex } = this.state;
-    const { headings } = this.props;
-    return headings.map(
-      (heading, index): React.ReactNode => {
-        const selected = selectedIndex === index;
-        const borderBottomWidth = `border-b-${selected ? '4' : '2'}`;
-        const borderBottomColor = `border-${selected ? 'selected-item' : 'primary-text'}`;
-        const onClick = (): void => {
-          this.setState({
-            selectedIndex: index,
-          });
-        };
-        return (
-          <button
-            className={`p-1 outline-none border-0 text-lg my-0 mx-2 text-primary-text border-solid ${borderBottomWidth} ${borderBottomColor}`}
-            type="button"
-            key={heading}
-            onClick={onClick}
-          >
-            {heading}
-          </button>
-        );
-      },
-    );
-  }
-
-  public render(): React.ReactNode {
-    const { children, headingChildren } = this.props;
-    const { selectedIndex } = this.state;
-
-    return (
-      <>
-        <div className="flex justify-between header">
-          <ul className="flex px-0 pt-2 pb-4">{this.renderHeadings()}</ul>
-          {headingChildren}
-        </div>
-        <div className="relative flex flex-col flex-grow w-full h-full panels">{children[selectedIndex]}</div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className="flex justify-between header">
+        <ul className="flex px-0 pt-2 pb-4">{renderHeadings(headings, selectedIndex, onClick)}</ul>
+        {headingChildren}
+      </div>
+      <div className="relative flex flex-col flex-grow w-full h-full panels">{children[selectedIndex]}</div>
+    </>
+  );
+};
 
 export default Tabs;

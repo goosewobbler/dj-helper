17d02c59a49635926e41c8c5ac9b81c5bc771f71
Sam Maister <sam.maister@bbc.co.uk>
2020-02-21 01:38:54 +0000

    first attempt at fixing up graph

--- src/renderer/components/ComponentDetails/Graph/index.tsx
+++ src/renderer/components/ComponentDetails/Graph/index.tsx
@@ -3,17 +3,12 @@ import * as React from 'react';
 import GraphVis from 'react-graph-vis';
 
 import { GraphData } from '../../../../common/types';
-import LoadingIcon from '../../LoadingIcon';
+import { LoadingIcon } from '../../LoadingIcon';
 
 type GraphProps = {
-  down: boolean;
   type: 'dependency' | 'dependant';
   componentName: string;
   onSelect(name: string): void;
-};
-
-type GraphState = {
-  data: GraphData | null;
 };
 
 const convertData = (data: GraphData): {} => {
@@ -24,129 +19,135 @@ const convertData = (data: GraphData): {} => {
   };
 };
 
-class Graph extends React.PureComponent<GraphProps, GraphState> {
-  public constructor(props: GraphProps) {
-    super(props);
+const renderLoadingIcon = (): React.ReactNode => (
+  <div className="flex items-center justify-center w-full h-full loading">
+    <div className="w-8 h-8">
+      <LoadingIcon />
+    </div>
+  </div>
+);
 
-    this.state = {
-      data: null,
-    };
-  }
-
-  public componentDidMount(): void {
-    this.update();
-  }
-
-  // public componentWillReceiveProps(nextProps: GraphProps): void {
-  //   const { url } = this.props;
-  //   if (nextProps.url !== url) {
-  //     this.setState({ data: null });
-  //   }
-  // }
-
-  public componentDidUpdate(): void {
-    const { data } = this.state;
-    if (!data) {
-      this.update();
-    }
-  }
-
-  private update(): void {
-    const { componentName, type } = this.props;
-    ipcRenderer.once(`${type}-graph`, (event, graphData: GraphData): void => {
-      this.setState({ data: graphData });
-    });
-    ipcRenderer.send(`get-${type}-graph`, componentName);
-  }
-
-  public renderGraph(data: GraphData): React.ReactElement {
-    const { down, onSelect } = this.props;
-    const options = {
-      edges: {
-        arrows: {
-          from: {
-            enabled: !down,
-            scaleFactor: 0.5,
-          },
-          to: {
-            enabled: down,
-            scaleFactor: 0.5,
-          },
+const renderGraph = (data: GraphData, onSelect: GraphProps['onSelect'], type: GraphProps['type']): React.ReactNode => {
+  console.log('rendering graph with data', data);
+  const isDependencyGraph = type === 'dependency';
+  const options = {
+    edges: {
+      arrows: {
+        from: {
+          enabled: !isDependencyGraph,
+          scaleFactor: 0.5,
         },
-        smooth: {
-          forceDirection: 'vertical',
-          roundness: 1,
-          type: 'cubicBezier',
+        to: {
+          enabled: isDependencyGraph,
+          scaleFactor: 0.5,
         },
       },
-      layout: {
-        hierarchical: {
-          direction: down ? 'UD' : 'DU',
-          edgeMinimization: true,
-          nodeSpacing: 350,
-          parentCentralization: true,
-          sortMethod: 'directed',
-        },
-        improvedLayout: true,
+      smooth: {
+        forceDirection: 'vertical',
+        roundness: 1,
+        type: 'cubicBezier',
       },
-      nodes: {
-        color: {},
-        font: {},
-        scaling: {
-          max: 30,
-          min: 30,
-        },
-        shape: 'box',
-        size: 30,
+    },
+    layout: {
+      hierarchical: {
+        direction: isDependencyGraph ? 'UD' : 'DU',
+        edgeMinimization: true,
+        nodeSpacing: 350,
+        parentCentralization: true,
+        sortMethod: 'directed',
       },
-      physics: {
-        enabled: false,
+      improvedLayout: true,
+    },
+    nodes: {
+      color: {},
+      font: {},
+      scaling: {
+        max: 30,
+        min: 30,
       },
-    };
+      shape: 'box',
+      size: 30,
+    },
+    physics: {
+      enabled: false,
+    },
+  };
 
-    const events = {
-      doubleClick: (event: { nodes: number[] }): void => {
-        const {
-          nodes: [id],
-        } = event;
-        const { name } = data.nodes.find((n): boolean => n.id === id)!;
-        onSelect(name);
-      },
-    };
+  const events = {
+    doubleClick: (event: { nodes: number[] }): void => {
+      const {
+        nodes: [id],
+      } = event;
+      const { name } = data.nodes.find((n): boolean => n.id === id)!;
+      onSelect(name);
+    },
+  };
 
-    const currentNode = data.nodes[0].id;
-
-    return (
-      <GraphVis
-        getNetwork={(network: { focus: Function }): void => {
+  return (
+    <GraphVis
+      getNetwork={(network: { focus: Function }): void => {
+        if (data.nodes.length > 0) {
+          const currentNode = data.nodes[0].id;
           network.focus(currentNode, {
             scale: 1,
           });
-        }}
-        graph={convertData(data)}
-        events={events}
-        options={options}
-      />
-    );
-  }
+        }
+      }}
+      graph={convertData(data)}
+      events={events}
+      options={options}
+    />
+  );
+};
 
-  public render(): React.ReactElement {
-    const { data } = this.state;
-    return (
-      <div className="absolute top-0 bottom-0 left-0 right-0">
-        {data ? (
-          this.renderGraph(data)
-        ) : (
-          <div className="flex items-center justify-center w-full h-full loading">
-            <div className="w-8 h-8">
-              <LoadingIcon />
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+const Graph = ({ onSelect, componentName, type }: GraphProps): React.ReactElement => {
+  const graphDataRequested = React.useRef<boolean>();
+  const [data, setData] = React.useState();
+  React.useEffect(() => {
+    const graphDataListener = (event: Event, graphData: GraphData): void => {
+      console.log('received graph data', graphData);
+      // {edges: Array(0), nodes: Array(0)}
+      setData(graphData);
+    };
+    if (!graphDataRequested.current) {
+      ipcRenderer.once(`${type}-graph`, graphDataListener);
+      ipcRenderer.send(`get-${type}-graph`, componentName);
+      graphDataRequested.current = true;
+    }
+
+    return (): void => {
+      ipcRenderer.removeAllListeners(`${type}-graph`);
+    };
+  });
+
+  console.log('render');
+
+  return (
+    <div className="absolute top-0 bottom-0 left-0 right-0">
+      {data ? renderGraph(data, onSelect, type) : renderLoadingIcon()}
+    </div>
+  );
+};
+
+// class Graph extends React.PureComponent<GraphProps, GraphState> {
+//   public componentDidMount(): void {
+//     this.update();
+//   }
+
+//   public componentWillReceiveProps(nextProps: GraphProps): void {
+//     const { url } = this.props;
+//     if (nextProps.url !== url) {
+//       this.setState({ data: null });
+//     }
+//   }
+
+//   public componentDidUpdate(): void {
+//     const { data } = this.state;
+//     if (!data) {
+//       this.update();
+//     }
+//   }
+// }
 
 export default Graph;
 export { GraphProps };

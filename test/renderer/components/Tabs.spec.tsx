92a169aee81f6e5b489557b0d936ca000e5acec4
Sam Maister <sam.maister@bbc.co.uk>
2020-02-21 10:33:38 +0000

    bye enzyme

--- test/renderer/components/Tabs.spec.tsx
+++ test/renderer/components/Tabs.spec.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
+import { render, RenderResult, fireEvent } from '@testing-library/react';
 import Tabs from '../../../src/renderer/components/ComponentDetails/Tabs';
 
-let tabs: ReactWrapper;
+let tabs: RenderResult;
+
+afterEach(() => {
+  tabs.unmount();
+});
 
 describe('given a list of tab headings', () => {
   let headings: string[];
 
   beforeEach(() => {
     headings = ['look', 'some', 'headings'];
-    tabs = mount(
+    tabs = render(
       <Tabs headings={headings}>
         <div className="child">I am child 1</div>
         <div className="child">I am child 2</div>
@@ -18,76 +22,52 @@ describe('given a list of tab headings', () => {
     );
   });
 
-  afterEach(() => {
-    tabs.unmount();
-  });
-
   it('should render tab buttons with the expected text', () => {
-    expect(
-      tabs
-        .find('button')
-        .at(0)
-        .text(),
-    ).toEqual(headings[0]);
-    expect(
-      tabs
-        .find('button')
-        .at(1)
-        .text(),
-    ).toEqual(headings[1]);
-    expect(
-      tabs
-        .find('button')
-        .at(2)
-        .text(),
-    ).toEqual(headings[2]);
+    expect(tabs.getByTestId('header')).toContainElement(tabs.getByText('look'));
+    expect(tabs.getByTestId('header')).toContainElement(tabs.getByText('some'));
+    expect(tabs.getByTestId('header')).toContainElement(tabs.getByText('headings'));
   });
 
   it('should render the expected panel', () => {
-    expect(tabs.find('.panels').contains(<div className="child">I am child 1</div>)).toEqual(true);
+    expect(tabs.getByTestId('panels')).toContainElement(tabs.getByText('I am child 1'));
   });
 
   it('should render the expected html', () => {
-    expect(tabs.html()).toMatchSnapshot();
-  });
-
-  describe('and heading children are provided', () => {
-    beforeEach(() => {
-      tabs = mount(
-        <Tabs
-          headings={['so', 'many', 'headings']}
-          headingChildren={<div className="heading-child">I am heading child 1</div>}
-        >
-          <div className="child">I am child 1</div>
-          <div className="child">I am child 2</div>
-          <div className="child">I am child 3</div>
-        </Tabs>,
-      );
-    });
-
-    it('should render the expected heading children', () => {
-      expect(tabs.find('.header').contains(<div className="heading-child">I am heading child 1</div>)).toEqual(true);
-    });
-
-    it('should render the expected html', () => {
-      expect(tabs.html()).toMatchSnapshot();
-    });
+    expect(tabs.container).toMatchSnapshot();
   });
 
   describe('when a tab button is clicked', () => {
     beforeEach(() => {
-      tabs
-        .find('button')
-        .at(2)
-        .simulate('click');
+      const button = tabs.getByText('headings');
+      fireEvent.click(button);
     });
 
     it('should render the expected panel', () => {
-      expect(tabs.find('.panels').contains(<div className="child">I am child 3</div>)).toEqual(true);
+      expect(tabs.getByTestId('panels')).toContainElement(tabs.getByText('I am child 3'));
     });
 
     it('should render the expected html', () => {
-      expect(tabs.html()).toMatchSnapshot();
+      expect(tabs.container).toMatchSnapshot();
     });
+  });
+});
+
+describe('and heading children are provided', () => {
+  beforeEach(() => {
+    tabs = render(
+      <Tabs headings={['so', 'many', 'headings']} headingChildren={<div>I am heading child</div>}>
+        <div className="child">I am child 1</div>
+        <div className="child">I am child 2</div>
+        <div className="child">I am child 3</div>
+      </Tabs>,
+    );
+  });
+
+  it('should render the expected heading children', () => {
+    expect(tabs.getByTestId('header')).toContainElement(tabs.getByText('I am heading child'));
+  });
+
+  it('should render the expected html', () => {
+    expect(tabs.container).toMatchSnapshot();
   });
 });

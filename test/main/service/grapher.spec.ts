06f24ddca452281e7543613602011da14e7b7e52
Sam Maister <sam.maister@bbc.co.uk>
2020-02-21 01:36:13 +0000

    first attempt at grapher tests

---
+++ test/main/service/grapher.spec.ts
@@ -0,0 +1,129 @@
+import { Grapher, createGrapher } from '../../../src/main/service/grapher';
+
+let grapher: Grapher;
+
+beforeEach(() => {
+  grapher = createGrapher({
+    'bbc-morph-foo': [{ name: 'bbc-morph-bar' }, { name: 'bbc-morph-baz' }],
+    'bbc-morph-bar': [{ name: 'bbc-morph-baz' }],
+    'bbc-morph-no-dependencies': [],
+  });
+});
+
+it('should return dependant graph data', () => {
+  const dependantData = grapher.getDependantData('bbc-morph-foo');
+
+  expect(dependantData).toEqual({
+    edges: [
+      {
+        from: 2,
+        to: 0,
+      },
+    ],
+    nodes: [
+      {
+        id: 2,
+        name: 'bbc-morph-bar',
+      },
+      {
+        id: 0,
+        name: 'bbc-morph-baz',
+      },
+    ],
+  });
+});
+
+// test('can get dependency graph data from service', async () => {
+//   const { service } = await createMockService();
+
+//   const dependantData = await service.getDependencyGraph('bbc-morph-foo');
+
+//   expect(dependantData).toEqual({
+//     edges: [
+//       {
+//         from: 0,
+//         to: 1,
+//       },
+//       {
+//         from: 0,
+//         to: 2,
+//       },
+//     ],
+//     nodes: [
+//       {
+//         id: 0,
+//         name: 'bbc-morph-foo',
+//       },
+//       {
+//         id: 1,
+//         name: 'bbc-morph-bar',
+//       },
+//       {
+//         id: 2,
+//         name: 'bbc-morph-baz',
+//       },
+//     ],
+//   });
+// });
+
+// test('shared parent', () => {
+//   const grapher = Grapher({
+//     a: [{ name: 'b1' }, { name: 'b2' }],
+//     b1: [{ name: 'c' }],
+//     b2: [{ name: 'c' }],
+//     c: [],
+//   });
+
+//   expect(grapher.getDependencyData('a')).toEqual({
+//     edges: [
+//       { from: 0, to: 1 },
+//       { from: 1, to: 3 },
+//       { from: 0, to: 2 },
+//       { from: 2, to: 3 },
+//     ],
+//     nodes: [
+//       { id: 0, name: 'a' },
+//       { id: 1, name: 'b1' },
+//       { id: 3, name: 'c' },
+//       { id: 2, name: 'b2' },
+//     ],
+//   });
+
+//   expect(grapher.getDependantData('c')).toEqual({
+//     edges: [
+//       { from: 3, to: 1 },
+//       { from: 1, to: 0 },
+//       { from: 3, to: 2 },
+//       { from: 2, to: 0 },
+//     ],
+//     nodes: [
+//       { id: 3, name: 'c' },
+//       { id: 1, name: 'b1' },
+//       { id: 0, name: 'a' },
+//       { id: 2, name: 'b2' },
+//     ],
+//   });
+// });
+
+// test('missing dependency', () => {
+//   const grapher = Grapher({
+//     a: [{ name: 'b1' }, { name: 'b2' }],
+//     b1: [{ name: 'd' }],
+//     b2: [{ name: 'd' }],
+//     c: [],
+//   });
+
+//   expect(grapher.getDependencyData('a')).toEqual({
+//     edges: [
+//       { from: 0, to: 1 },
+//       { from: 0, to: 2 },
+//     ],
+//     nodes: [
+//       { id: 0, name: 'a' },
+//       { id: 1, name: 'b1' },
+//       { id: 2, name: 'b2' },
+//     ],
+//   });
+
+//   expect(grapher.getDependantData('c')).toEqual({ edges: [], nodes: [{ id: 3, name: 'c' }] });
+// });

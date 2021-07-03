b71d910fb2e4776d514adcc216478a643d49c1e4
goosewobbler <goosewobbler@pm.me>
2021-07-03 20:44:08 +0100

    add types

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,14 +1,27 @@
 /* eslint global-require: off */
+
+/**
+ * @type {import("../src/common/types").LooseObject}
+ */
 const theme = {
   extend: {},
 };
 
+/**
+ * @type {import("../src/common/types").LooseObject}
+ */
 const variants = {
   borderColor: ['responsive', 'hover', 'focus', 'focus-within', 'active'],
 };
 
+/**
+ * @type {unknown[]}
+ */
 const plugins = [];
 
+/**
+ * @type {string[]}
+ */
 const purge = ['./src/**/*.ts{,x}', './src/index.ejs'];
 
 module.exports = {

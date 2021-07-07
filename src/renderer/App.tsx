2e7b1c86a248db73499db0da54d493822210045a
goosewobbler <goosewobbler@pm.me>
2021-07-07 15:15:01 +0100

    add tabbed browsers

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,14 @@
 import React, { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
+import { Browser } from '../common/types';
 import { BrowserPane } from '../features/browser/BrowserPane';
+import { selectBrowser } from '../features/browser/browserSlice';
+import { Tabs } from '../features/browser/Tabs';
 import { ListPane } from '../features/lists/ListPane';
 
 export const App = (): ReactElement => {
+  const browsers = useSelector(selectBrowser);
+  const tabHeadings = browsers.map((browser) => browser.title);
   return (
     <div className="flex flex-col flex-grow bg-primary-background">
       <div className="flex items-center justify-between flex-shrink-0 p-3 border-b shadow-md header bg-header-5">
@@ -15,7 +21,15 @@ export const App = (): ReactElement => {
           <ListPane />
         </div>
         <div className="flex flex-col flex-grow flex-shrink-0 w-2/3 p-2 section">
-          <BrowserPane />
+          <div className="flex flex-col flex-grow">
+            <Tabs headings={tabHeadings}>
+              {browsers.map(
+                (browser: Browser): ReactElement => (
+                  <BrowserPane browser={browser} />
+                ),
+              )}
+            </Tabs>
+          </div>
         </div>
       </div>
     </div>

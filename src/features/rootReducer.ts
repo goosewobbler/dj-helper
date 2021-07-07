540ad3512bf9f96f0f43514dcfe8fbf99b575567
goosewobbler <goosewobbler@pm.me>
2021-07-07 20:02:17 +0100

    bring in new reducers

--- src/features/rootReducer.ts
+++ src/features/rootReducer.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { listsReducer } from './lists/listsSlice';
-// import { tracksReducer } from './tracks/tracksSlice';
+import { tracksReducer } from './tracks/tracksSlice';
+import { browsersReducer } from './browsers/browsersSlice';
 // import { trackSourcesReducer } from './trackSources/trackSourcesSlice';
 
 export const rootReducer = combineReducers({
   lists: listsReducer,
-  // tracks: tracksReducer,
+  tracks: tracksReducer,
+  browsers: browsersReducer,
   // trackSources: trackSourcesReducer,
 });
 

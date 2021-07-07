9e475ef8de10b8ad3d810a7ed3476969d69d095d
goosewobbler <goosewobbler@pm.me>
2021-07-07 15:12:33 +0100

    basic track display

--- src/features/browser/MetaPanel.tsx
+++ src/features/browser/MetaPanel.tsx
@@ -1,5 +1,15 @@
 import React, { ReactElement } from 'react';
+import { Track } from '../../common/types';
 
-export function MetaPanel(): ReactElement {
-  return <div />;
+export function MetaPanel({ tracks }: { tracks: Track[] }): ReactElement {
+  return (
+    <div>
+      {tracks.map((track) => (
+        <div>
+          <span>{track.artist}</span>
+          <span>{track.title}</span>
+        </div>
+      ))}
+    </div>
+  );
 }

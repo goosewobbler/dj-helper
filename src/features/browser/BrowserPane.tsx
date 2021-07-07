3b9b08b033ecc79c3a01e87fe02ba419ee22a5d7
goosewobbler <goosewobbler@pm.me>
2021-07-07 15:14:26 +0100

    pass tracks through to meta

--- src/features/browser/BrowserPane.tsx
+++ src/features/browser/BrowserPane.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
+import { Browser } from '../../common/types';
 import { MetaPanel } from './MetaPanel';
 
-export function BrowserPane(): ReactElement {
+export function BrowserPane({ browser }: { browser: Browser }): ReactElement {
+  const browserTracks = browser.tracks;
   return (
     <div>
-      <MetaPanel />
+      <MetaPanel tracks={browserTracks} />
       <div className="browserPanel" />
     </div>
   );

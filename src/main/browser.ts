14b2096652a6548bce0ab96f2eca07704fef629b
goosewobbler <goosewobbler@pm.me>
2021-07-07 20:06:30 +0100

    attempt #2 at creating tracks

--- src/main/browser.ts
+++ src/main/browser.ts
@@ -1,45 +1,54 @@
 import { BrowserView, BrowserWindow } from 'electron';
-import { useSelector, useDispatch } from 'react-redux';
-import { clearTracks, selectBrowser } from '../features/browser/browserSlice';
-import { createTrack, TrackData } from '../features/tracks/tracksSlice';
+import { Store } from '@reduxjs/toolkit';
+import { createTrack, TrackData, unlinkBrowserFromTracks } from '../features/tracks/tracksSlice';
 import { parseBandcampPageData } from './helpers/bandcamp';
+import { Browser } from '../common/types';
+import { log } from './helpers/console';
 
-export function initBrowsers(mainWindow: BrowserWindow) {
-  const browsers = useSelector(selectBrowser);
-  const dispatch = useDispatch();
+export function initBrowsers(mainWindow: BrowserWindow, { dispatch, getState }: Store) {
+  const state = getState();
 
-  browsers.forEach((browser) => {
+  log(state.browsers);
+
+  state.browsers.forEach((browser: Browser) => {
     const view = new BrowserView();
     mainWindow.setBrowserView(view);
-    view.setBounds({ x: 300, y: 65, width: 1200, height: 550 });
+    view.setBounds({ x: 300, y: 165, width: 1200, height: 550 }); // y: 65
     view.setAutoResize({ horizontal: true });
     void view.webContents.loadURL(browser.url);
 
-    view.webContents.on('new-window', (event, url) => {
-      event.preventDefault();
+    view.webContents.setWindowOpenHandler(({ url }) => {
       view.webContents.loadURL(url);
+      return { action: 'deny' };
     });
 
-    view.webContents.on('did-start-navigation', (event, url) => {
-      dispatch(clearTracks(browser.id));
+    view.webContents.on('did-finish-load', () => {
+      const url = view.webContents.getURL();
+      log('loaded url', url);
+      dispatch(unlinkBrowserFromTracks({ browserId: browser.id })); // unlink browser from tracks
       if (url.match(/bandcamp.com\/track|album/)) {
-        view.webContents.once('did-finish-load', () => {
-          (async () => {
-            const [TralbumData, BandData, TralbumCollectInfo, bandCurrency] = await view.webContents.executeJavaScript(
-              '[ TralbumData, BandData, TralbumCollectInfo, bandCurrency ]',
-              true,
-            );
-            const bcPageData = parseBandcampPageData(TralbumData, BandData, TralbumCollectInfo, bandCurrency, url);
-            console.log(bcPageData);
-            bcPageData.trackinfo.forEach(({ title, artist, duration }) => {
-              const trackData: TrackData = { title, artist, duration, url, priceCurrency: bcPageData.currency };
-              if (url.match(/bandcamp.com\/track/)) {
-                trackData.price = bcPageData.price;
-              }
-              dispatch(createTrack(trackData));
-            });
-          })();
-        });
+        log('url is bandcamp album or track');
+        (async () => {
+          const [TralbumData, BandData, TralbumCollectInfo, bandCurrency] = await view.webContents.executeJavaScript(
+            '[ TralbumData, BandData, TralbumCollectInfo, bandCurrency ]',
+            true,
+          );
+          const bcPageData = parseBandcampPageData(TralbumData, BandData, TralbumCollectInfo, bandCurrency, url);
+          log('parsed bandcamp page data');
+          bcPageData.trackinfo.forEach(({ title, artist, duration }) => {
+            // pass price where we have it, check url
+            const trackData: TrackData = {
+              title,
+              artist,
+              duration,
+              browserId: browser.id,
+              url,
+              priceCurrency: bcPageData.currency,
+            };
+            log('creating track', trackData);
+            dispatch(createTrack(trackData));
+          });
+        })();
       }
     });
   });

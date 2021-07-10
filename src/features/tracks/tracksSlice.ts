b7556419e41e7766053d53b2132bb8da0f9aee72
goosewobbler <goosewobbler@pm.me>
2021-07-10 12:49:23 +0100

    clean this slice up a bit

--- src/features/tracks/tracksSlice.ts
+++ src/features/tracks/tracksSlice.ts
@@ -5,6 +5,7 @@ import { log } from '../../main/helpers/console';
 const initialState: Track[] = [];
 
 export type TrackData = {
+  id?: Track['id'];
   title: Track['title'];
   artist: Track['artist'];
   duration: Track['duration'];
@@ -58,44 +59,56 @@ export const slice = createSlice({
   name: 'tracks',
   initialState,
   reducers: {
-    createTrack: (state, { payload: { title, artist, duration, browserId, url, price, priceCurrency } }) => {
-      let trackToUpdate = getTrackDuplicate(state, title, artist, duration);
+    createTrack: (
+      state,
+      { payload: { title, artist, duration, browserId, url, price, priceCurrency } }: { payload: TrackData },
+    ) => {
+      const trackToUpdate = getTrackDuplicate(state, title, artist, duration);
       if (trackToUpdate) {
         log('found track dupe, updating track sources');
         updateTrackSources(trackToUpdate, url, price, priceCurrency);
-      } else {
-        const newTrack = {
-          id: state.length + 1,
-          title,
-          artist,
-          duration,
-          browserId,
-          sources: [],
-        };
-        addTrackSource(newTrack, url, price, priceCurrency);
-        log('creating new track', newTrack);
-        state.push(newTrack);
+        const updatedIndex = state.findIndex((track) => track === trackToUpdate);
+        return [...state.slice(0, updatedIndex - 1), trackToUpdate, ...state.slice(updatedIndex + 1)];
       }
+      const newTrack: Track = {
+        id: state.length + 1,
+        title,
+        artist,
+        duration,
+        browserId,
+        sources: [],
+      };
+      addTrackSource(newTrack, url, price, priceCurrency);
+      // log('creating new track', newTrack);
+      return [...state, newTrack];
     },
-    updateTrack: (state, { payload: { id, browserId, title, artist, duration, url, price, priceCurrency } }) => {
-      const trackToUpdate = state.find((track) => track.id === id);
-      if (trackToUpdate) {
-        if (title) {
-          trackToUpdate.title = title;
-        }
-        if (artist) {
-          trackToUpdate.artist = artist;
-        }
-        if (duration) {
-          trackToUpdate.duration = duration;
-        }
-        if (browserId) {
-          trackToUpdate.browserId = browserId;
-        }
-        if (url) {
-          updateTrackSources(trackToUpdate, url, price, priceCurrency);
-        }
+    updateTrack: (
+      state,
+      { payload: { id, browserId, title, artist, duration, url, price, priceCurrency } }: { payload: TrackData },
+    ) => {
+      const indexToUpdate = state.findIndex((track) => track.id === id);
+      const trackToUpdate = state[indexToUpdate];
+      if (!trackToUpdate) {
+        return state;
       }
+
+      const updatedTrack = { ...trackToUpdate };
+      if (title) {
+        updatedTrack.title = title;
+      }
+      if (artist) {
+        updatedTrack.artist = artist;
+      }
+      if (duration) {
+        updatedTrack.duration = duration;
+      }
+      if (browserId) {
+        updatedTrack.browserId = browserId;
+      }
+      if (url) {
+        updateTrackSources(updatedTrack, url, price, priceCurrency);
+      }
+      return [...state.slice(0, indexToUpdate - 1), trackToUpdate, ...state.slice(indexToUpdate + 1)];
     },
     deleteTrack: (state, { payload }) => {
       const indexToRemove = state.findIndex((track) => track.id === payload);
@@ -107,10 +120,11 @@ export const slice = createSlice({
           track.id -= 1;
         });
       }
+      return state;
     },
     unlinkBrowserFromTracks: (state, { payload: { browserId } }) => {
       log('unlinking tracks', browserId);
-      state.filter((track) => track.browserId === browserId).forEach((track) => delete track.browserId);
+      return state.filter((track) => track.browserId === browserId).forEach((track) => delete track.browserId);
     },
   },
 });

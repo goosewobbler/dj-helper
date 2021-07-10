af367690a7b6179b9422a918d162d2f0b64dc1eb
goosewobbler <goosewobbler@pm.me>
2021-07-10 14:48:48 +0100

    nicer async app creation

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -5,12 +5,14 @@ import { log } from './helpers/console';
 
 const currentVersion = process.env.npm_package_version as string;
 
-export const createApp = (mainWindow: Electron.BrowserWindow, isDev: boolean): void => {
-  const reduxStore = createReduxStore({ context: 'main', syncFn: syncMain, persistCallback: () => {} });
+export const createApp = async (mainWindow: Electron.BrowserWindow, isDev: boolean): Promise<void> => {
+  const reduxStore = await createReduxStore({ context: 'main', syncFn: syncMain });
 
   initBrowsers(mainWindow, reduxStore);
 
   // ipcMain.handle('get-store-value', (event, key): LooseObject => store.get(key));
 
   log(`DJ Helper v${currentVersion} is starting${isDev ? ' in development mode...' : '...'}`);
+
+  return Promise.resolve();
 };

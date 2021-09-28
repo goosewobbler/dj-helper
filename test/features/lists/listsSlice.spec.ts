46263b7a77e72d7078a24a14a66b29176058b703
goosewobbler <goosewobbler@pm.me>
2021-09-28 13:30:28 +0100

    add track up / down tests

--- test/features/lists/listsSlice.spec.ts
+++ test/features/lists/listsSlice.spec.ts
@@ -1,12 +1,21 @@
 import { reducers } from '../../../src/features/lists/listsSlice';
 
-const { createList, deleteList, updateListTitle, editList, finishEditList, revertEditList } = reducers;
+const {
+  createList,
+  deleteList,
+  updateListTitle,
+  editList,
+  finishEditList,
+  revertEditList,
+  moveTrackUp,
+  moveTrackDown,
+} = reducers;
 
 describe('listsSlice', () => {
   describe('createList', () => {
     it('should create a new list with the expected values', () => {
       const lists = createList([]);
-      expect(lists).toEqual([{ id: 1, editing: true, title: 'New List', tracks: [] }]);
+      expect(lists).toEqual([{ id: 1, active: true, editing: true, title: 'New List', tracks: [] }]);
     });
 
     describe('when there are existing lists', () => {
@@ -18,7 +27,7 @@ describe('listsSlice', () => {
         expect(lists).toEqual([
           { id: 1, title: 'first list', tracks: [] },
           { id: 2, title: 'second list', tracks: [] },
-          { id: 3, editing: true, title: 'New List', tracks: [] },
+          { id: 3, active: true, editing: true, title: 'New List', tracks: [] },
         ]);
       });
     });
@@ -115,6 +124,42 @@ describe('listsSlice', () => {
       expect(lists).toEqual([
         { id: 1, title: 'first list', tracks: [] },
         { id: 2, title: 'New List', tracks: [] },
+        { id: 3, title: 'third list', tracks: [] },
+      ]);
+    });
+  });
+
+  describe('moveTrackUp', () => {
+    it('should move a track up', () => {
+      const lists = moveTrackUp(
+        [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+        { payload: { trackId: 3 } },
+      );
+      expect(lists).toEqual([
+        { id: 1, title: 'first list', active: true, tracks: [1, 3, 2] },
+        { id: 2, title: 'second list', tracks: [] },
+        { id: 3, title: 'third list', tracks: [] },
+      ]);
+    });
+  });
+
+  describe('moveTrackDown', () => {
+    it('should move a track down', () => {
+      const lists = moveTrackDown(
+        [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+        { payload: { trackId: 1 } },
+      );
+      expect(lists).toEqual([
+        { id: 1, title: 'first list', active: true, tracks: [2, 1, 3] },
+        { id: 2, title: 'second list', tracks: [] },
         { id: 3, title: 'third list', tracks: [] },
       ]);
     });

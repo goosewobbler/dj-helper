136b0bef13fc8ef4541f9b569d068184dcc640d3
goosewobbler <goosewobbler@pm.me>
2021-09-28 17:23:32 +0100

    finish listSlice tests for new things

--- test/features/lists/listsSlice.spec.ts
+++ test/features/lists/listsSlice.spec.ts
@@ -1,4 +1,4 @@
-import { reducers } from '../../../src/features/lists/listsSlice';
+import { reducers, trackIsOnSelectedList, getNextTrackOnList } from '../../../src/features/lists/listsSlice';
 
 const {
   createList,
@@ -9,6 +9,8 @@ const {
   revertEditList,
   moveTrackUp,
   moveTrackDown,
+  addTrackToSelectedList,
+  removeTrackFromSelectedList,
 } = reducers;
 
 describe('listsSlice', () => {
@@ -145,6 +147,22 @@ describe('listsSlice', () => {
         { id: 3, title: 'third list', tracks: [] },
       ]);
     });
+
+    it('should handle a non-existent track and return the existing order', () => {
+      const lists = moveTrackUp(
+        [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+        { payload: { trackId: 5 } },
+      );
+      expect(lists).toEqual([
+        { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+        { id: 2, title: 'second list', tracks: [] },
+        { id: 3, title: 'third list', tracks: [] },
+      ]);
+    });
   });
 
   describe('moveTrackDown', () => {
@@ -162,6 +180,152 @@ describe('listsSlice', () => {
         { id: 2, title: 'second list', tracks: [] },
         { id: 3, title: 'third list', tracks: [] },
       ]);
+    });
+
+    it('should handle a non-existent track and return the existing order', () => {
+      const lists = moveTrackDown(
+        [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+        { payload: { trackId: 5 } },
+      );
+      expect(lists).toEqual([
+        { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+        { id: 2, title: 'second list', tracks: [] },
+        { id: 3, title: 'third list', tracks: [] },
+      ]);
+    });
+  });
+
+  describe('addTrackToSelectedList', () => {
+    it('should add a track to the selected list', () => {
+      const lists = addTrackToSelectedList(
+        [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+        { payload: { trackId: 5 } },
+      );
+      expect(lists).toEqual([
+        { id: 1, title: 'first list', active: true, tracks: [1, 2, 3, 5] },
+        { id: 2, title: 'second list', tracks: [] },
+        { id: 3, title: 'third list', tracks: [] },
+      ]);
+    });
+  });
+
+  describe('removeTrackFromSelectedList', () => {
+    it('should add a track to the selected list', () => {
+      const lists = removeTrackFromSelectedList(
+        [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+        { payload: { trackId: 2 } },
+      );
+      expect(lists).toEqual([
+        { id: 1, title: 'first list', active: true, tracks: [1, 3] },
+        { id: 2, title: 'second list', tracks: [] },
+        { id: 3, title: 'third list', tracks: [] },
+      ]);
+    });
+  });
+
+  describe('trackIsOnSelectedList', () => {
+    it('should return true when a track is on the selected list', () => {
+      const isOnListSelector = trackIsOnSelectedList({ trackId: 2 });
+      const isOnList = isOnListSelector({
+        embed: { isPlaying: false },
+        tracks: [],
+        browsers: [],
+        lists: [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+      });
+      expect(isOnList).toEqual(true);
+    });
+
+    it('should return false when a track is not on the selected list', () => {
+      const isOnListSelector = trackIsOnSelectedList({ trackId: 5 });
+      const isOnList = isOnListSelector({
+        embed: { isPlaying: false },
+        tracks: [],
+        browsers: [],
+        lists: [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+      });
+      expect(isOnList).toEqual(false);
+    });
+  });
+
+  describe('getNextTrackOnList', () => {
+    it('should return the next track on a list', () => {
+      const nextTrackSelector = getNextTrackOnList({ id: 1, currentTrackId: 2 });
+      const nextTrack = nextTrackSelector({
+        embed: { isPlaying: false },
+        tracks: [],
+        browsers: [],
+        lists: [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+      });
+      expect(nextTrack).toEqual(3);
+    });
+
+    it('should return the next track on an unselected list', () => {
+      const nextTrackSelector = getNextTrackOnList({ id: 1, currentTrackId: 2 });
+      const nextTrack = nextTrackSelector({
+        embed: { isPlaying: false },
+        tracks: [],
+        browsers: [],
+        lists: [
+          { id: 1, title: 'first list', tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', active: true, tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+      });
+      expect(nextTrack).toEqual(3);
+    });
+
+    it('should return the first track when the current track is not on the list', () => {
+      const nextTrackSelector = getNextTrackOnList({ id: 1, currentTrackId: 5 });
+      const nextTrack = nextTrackSelector({
+        embed: { isPlaying: false },
+        tracks: [],
+        browsers: [],
+        lists: [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+      });
+      expect(nextTrack).toEqual(1);
+    });
+
+    it('should return undefined when there is no next track', () => {
+      const nextTrackSelector = getNextTrackOnList({ id: 1, currentTrackId: 3 });
+      const nextTrack = nextTrackSelector({
+        embed: { isPlaying: false },
+        tracks: [],
+        browsers: [],
+        lists: [
+          { id: 1, title: 'first list', active: true, tracks: [1, 2, 3] },
+          { id: 2, title: 'second list', tracks: [] },
+          { id: 3, title: 'third list', tracks: [] },
+        ],
+      });
+      expect(nextTrack).toEqual(undefined);
     });
   });
 });

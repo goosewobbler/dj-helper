840f9b17521d7de0469f1971016d598ab0a80ec9
goosewobbler <goosewobbler@pm.me>
2021-10-07 14:39:00 +0100

    basic scrolling of meta

--- src/features/browsers/MetaPanel.tsx
+++ src/features/browsers/MetaPanel.tsx
@@ -6,7 +6,7 @@ import { TrackMeta } from '../tracks/TrackMeta';
 export function MetaPanel({ browser: { id, tracks } }: { browser: Browser }): ReactElement {
   log('tracks', tracks);
   return (
-    <div>
+    <div className="inline-block w-full overflow-scroll h-80">
       {tracks?.map((trackId) => (
         <TrackMeta key={trackId} id={trackId} context={`browser-${id}`} />
       ))}

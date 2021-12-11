98c778346c512cbc27425c7d61be5a4e71e70455
goosewobbler <goosewobbler@pm.me>
2021-12-11 01:36:16 +0000

    make switch generic

--- src/features/ui/Switch.tsx
+++ src/features/ui/Switch.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-export const Switch = ({ id, isOn, handleToggle }: { id: string; isOn: boolean; handleToggle: () => void }) => (
+export const Switch = ({
+  id,
+  isOn,
+  text,
+  handleToggle,
+}: {
+  id: string;
+  text: string;
+  isOn: boolean;
+  handleToggle: () => void;
+}) => (
   <div className="inline-block float-right mt-4 mr-4">
     <label className="switch-label" htmlFor={id}>
       <span className="switch-button" />
-      Small Embed
+      {text}
       <input checked={isOn} onChange={handleToggle} className="ml-1 switch-checkbox" id={id} type="checkbox" />
     </label>
   </div>

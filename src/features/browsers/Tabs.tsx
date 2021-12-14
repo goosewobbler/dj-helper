e87c5150e9a4da928e99a818a16deee82b593495
goosewobbler <goosewobbler@pm.me>
2021-12-14 23:16:01 +0000

    bye headlessui tabs

--- src/features/browsers/Tabs.tsx
+++ src/features/browsers/Tabs.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 import { batch } from 'react-redux';
-import { Tab } from '@headlessui/react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { useAppDispatch } from '../../common/hooks';
 import { createBrowser, deleteBrowser, tabSelected } from './browsersSlice';
 import { AppThunk, Browser, TabHistoryAction } from '../../common/types';
@@ -38,73 +38,86 @@ const clickCloseTabHandler =
     });
   };
 
-export const Tabs = ({ browsers, activeBrowser }: { browsers: Browser[]; activeBrowser: Browser }): ReactElement => {
+export const TabbedInterface = ({
+  browsers,
+  activeBrowser,
+}: {
+  browsers: Browser[];
+  activeBrowser: Browser;
+}): ReactElement => {
   const dispatch = useAppDispatch();
   const isSelected = (id: number) => id === activeBrowser.id;
+  const displayTabCloseButton = browsers.length > 1;
 
   return (
-    <Tab.Group
+    <Tabs
       defaultIndex={activeBrowser ? activeBrowser.id : 0}
-      onChange={(index) => {
+      onSelect={(index: number) => {
         batch(() => {
-          log('tab onChange', index);
-          dispatch(tabSelected({ id: index }));
-          dispatch(updateTabHistory({ tabId: index, action: TabHistoryAction.Selected }));
+          log('tab onSelect', index);
+          if (index !== activeBrowser.id) {
+            dispatch(tabSelected({ id: index }));
+            dispatch(updateTabHistory({ tabId: index, action: TabHistoryAction.Selected }));
+          }
         });
       }}
+      selectedIndex={activeBrowser.id}
     >
       <div className="flex justify-between" data-testid="header">
-        <Tab.List>
+        <TabList className="flex-grow">
           {browsers.map(
             (browser: Browser, index: number): ReactElement => (
-              <Tab key={browser.id}>
-                {() => (
+              <Tab
+                key={browser.id}
+                className={classNames(
+                  isSelected(browser.id)
+                    ? 'text-gray-900 cursor-default'
+                    : 'text-gray-500 hover:text-gray-700 cursor-pointer',
+                  'float-left rounded-r-lg rounded-l-lg w-80 overflow-hidden overflow-ellipsis whitespace-nowrap',
+                  'group relative min-w-0 flex-1 bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10',
+                )}
+              >
+                {displayTabCloseButton && (
                   <button
+                    className="absolute w-5 h-5 right-1"
                     type="button"
-                    className={classNames(
-                      isSelected(browser.id) ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
-                      'rounded-r-lg rounded-l-lg',
-                      'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10',
-                    )}
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      dispatch(clickCloseTabHandler(index, activeBrowser));
+                    }}
                   >
-                    <span>
-                      {browser.title}
-                      <button type="button" onClick={() => dispatch(clickCloseTabHandler(index, activeBrowser))}>
-                        x
-                      </button>
-                    </span>
-                    <span
-                      aria-hidden="true"
-                      className={classNames(
-                        isSelected(browser.id) ? 'bg-indigo-500' : 'bg-transparent',
-                        'absolute inset-x-0 bottom-0 h-0.5',
-                      )}
-                    />
+                    x
                   </button>
                 )}
+                <span className="overflow-hidden w-60 overflow-ellipsis whitespace-nowrap">{browser.title}</span>
+                <span
+                  aria-hidden="true"
+                  className={classNames(
+                    isSelected(browser.id) ? 'bg-indigo-500' : 'bg-transparent',
+                    'absolute inset-x-0 bottom-0 h-0.5',
+                  )}
+                />
               </Tab>
             ),
           )}
           <button
-            className="p-1 mx-2 my-0 text-lg border-0 border-solid outline-none text-primary-text"
+            className="p-1 mx-2 mt-2 text-lg border-0 border-solid outline-none text-primary-text"
             type="button"
             onClick={() => dispatch(clickNewTabHandler())}
           >
             +
           </button>
-        </Tab.List>
+        </TabList>
       </div>
       <div className="relative flex flex-col flex-grow w-full h-full" data-testid="panels">
-        <Tab.Panels>
-          {browsers.map(
-            (browser: Browser): ReactElement => (
-              <Tab.Panel key={browser.id}>
-                <MetaPanel browser={browser} />
-              </Tab.Panel>
-            ),
-          )}
-        </Tab.Panels>
+        {browsers.map(
+          (browser: Browser): ReactElement => (
+            <TabPanel key={browser.id} className="inline-block w-full overflow-y-scroll">
+              <MetaPanel browser={browser} />
+            </TabPanel>
+          ),
+        )}
       </div>
-    </Tab.Group>
+    </Tabs>
   );
 };

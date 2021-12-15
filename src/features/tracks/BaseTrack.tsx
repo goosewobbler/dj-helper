982efe0259f933958a81bc5c8d4f82a8bc5ee679
goosewobbler <goosewobbler@pm.me>
2021-12-15 20:39:52 +0000

    select-none on tracks

--- src/features/tracks/BaseTrack.tsx
+++ src/features/tracks/BaseTrack.tsx
@@ -36,7 +36,7 @@ export function BaseTrack({
   const isPlayingAdditionalStyles = isPlaying ? 'bg-blue-200' : '';
 
   return (
-    <div key={id} className={`group-track mb-1 px-2 py-1 ${isPlayingAdditionalStyles}`}>
+    <div key={id} className={`group-track select-none mb-1 px-2 py-1 ${isPlayingAdditionalStyles}`}>
       <span className="inline-block w-32 overflow-hidden whitespace-nowrap overflow-ellipsis">{artist}</span>
       <span className="inline-block overflow-hidden whitespace-nowrap overflow-ellipsis w-80">{title}</span>
       <span className="inline-block w-10 overflow-hidden whitespace-nowrap">{displayTrackDuration(duration)}</span>

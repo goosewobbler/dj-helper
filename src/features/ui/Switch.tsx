a0c6f8b393c2504c123c0e6b076f832178268250
goosewobbler <goosewobbler@pm.me>
2021-12-18 21:16:55 +0000

    remove focus ring

--- src/features/ui/Switch.tsx
+++ src/features/ui/Switch.tsx
@@ -19,14 +19,14 @@ export const Switch = ({
       onChange={handleToggle}
       className={classNames(
         isOn ? 'bg-indigo-600' : 'bg-gray-200',
-        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none',
       )}
     >
       <span
         aria-hidden="true"
         className={classNames(
           isOn ? 'translate-x-5' : 'translate-x-0',
-          'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+          'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200',
         )}
       />
     </UISwitch>

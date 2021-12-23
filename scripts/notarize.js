dc87bf6105a368580708ed77e00269eb16d3a04a
goosewobbler <goosewobbler@pm.me>
2021-12-23 04:00:31 +0000

    add notarize script

---
+++ scripts/notarize.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+const { notarize } = require('electron-notarize');
+
+module.exports = async function (context) {
+  const {
+    platformName,
+    appOutDir,
+    packager: { appInfo },
+  } = context;
+  // Only notarize the app on macOS.
+  if (platformName !== 'darwin') {
+    return;
+  }
+  console.log('afterSign hook triggered');
+
+  const { appId } = JSON.parse(await fs.promises.readFile('./electron-builder.json'));
+
+  const appPath = path.join(appOutDir, `${appInfo.productFilename}.app`);
+  await fs.promises.access(appPath);
+
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  try {
+    const appleIdPassword = `@keychain:AC_PASSWORD`;
+    await notarize({
+      appBundleId: appId,
+      appPath: appPath,
+      appleId: execSync('mas account'),
+      appleIdPassword,
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+
+  console.log(`Done notarizing ${appId}`);
+};

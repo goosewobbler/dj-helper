e7e8cfbc373533c94afe059ecb4e12963eda5c92
goosewobbler <goosewobbler@pm.me>
2021-12-26 16:30:50 +0000

    add Listbox example - to be implemented with new settings panel

---
+++ src/features/ui/ListBox.tsx
@@ -0,0 +1,92 @@
+import React, { Fragment, useState } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+
+const people = [
+  { name: 'Wade Cooper', username: '@wadecooper' },
+  { name: 'Arlene Mccoy', username: '@arlenemccoy' },
+  { name: 'Devon Webb', username: '@devonwebb' },
+  { name: 'Tom Cook', username: '@tomcook' },
+  { name: 'Tanya Fox', username: '@tanyafox' },
+  { name: 'Hellen Schmidt', username: '@hellenschmidt' },
+  { name: 'Caroline Schultz', username: '@carolineschultz' },
+  { name: 'Mason Heaney', username: '@masonheaney' },
+  { name: 'Claudie Smitham', username: '@claudiesmitham' },
+  { name: 'Emil Schaefer', username: '@emilschaefer' },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export function ListBox({ labelText }: { labelText: string }) {
+  const [selected, setSelected] = useState(people[3]);
+
+  return (
+    <Listbox value={selected} onChange={setSelected}>
+      {({ open }) => (
+        <>
+          <Listbox.Label className="block text-sm font-medium text-gray-700">{labelText}</Listbox.Label>
+          <div className="relative mt-1">
+            <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white border border-gray-300 rounded-md shadow-sm cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+              <span className="inline-flex w-full truncate">
+                <span className="truncate">{selected.name}</span>
+                <span className="ml-2 text-gray-500 truncate">{selected.username}</span>
+              </span>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <SelectorIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {people.map((person) => (
+                  <Listbox.Option
+                    key={person.username}
+                    className={({ active }) =>
+                      classNames(
+                        active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                        'cursor-default select-none relative py-2 pl-3 pr-9',
+                      )
+                    }
+                    value={person}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex">
+                          <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'truncate')}>
+                            {person.name}
+                          </span>
+                          <span className={classNames(active ? 'text-indigo-200' : 'text-gray-500', 'ml-2 truncate')}>
+                            {person.username}
+                          </span>
+                        </div>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? 'text-white' : 'text-indigo-600',
+                              'absolute inset-y-0 right-0 flex items-center pr-4',
+                            )}
+                          >
+                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
+  );
+}

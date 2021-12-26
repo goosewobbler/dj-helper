db162390a728a98f8d315ab4b87b02804cba1af8
goosewobbler <goosewobbler@pm.me>
2021-12-26 14:29:32 +0000

    switch to js electron-builder conf for a dynamic copyright message

---
+++ electron-builder.js
@@ -0,0 +1,31 @@
+module.exports = {
+  asar: true,
+  appId: 'com.dj-helper.app',
+  copyright: `Copyright © ${new Date().getFullYear()} Goosewobbler`,
+  productName: 'DJ Helper',
+  artifactName: '${name}-${version}-${os}-${arch}.${ext}',
+  afterSign: './scripts/notarize.js',
+  files: ['bundle/**/*', 'build/**/*'],
+  mac: {
+    category: 'public.app-category.music',
+    icon: 'build/icon.icns',
+    hardenedRuntime: true,
+    entitlements: './build/entitlements.mac.plist',
+    entitlementsInherit: './build/entitlements.mac.plist',
+  },
+  win: {
+    target: 'nsis',
+    icon: 'build/icon.ico',
+  },
+  nsis: {
+    deleteAppDataOnUninstall: true,
+  },
+  linux: {
+    category: 'Audio',
+    icon: 'build/icon/',
+    desktop: {
+      StartupWMClass: 'dj helper',
+    },
+    target: ['AppImage'],
+  },
+};

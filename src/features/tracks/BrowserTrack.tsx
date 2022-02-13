525918a3dab3ec22e931e57dfa822b12efd5178f
goosewobbler <goosewobbler@pm.me>
2022-02-13 15:32:14 +0000

    re-enable list toggle, ensure add/remove to list button hidden when no active list

--- src/features/tracks/BrowserTrack.tsx
+++ src/features/tracks/BrowserTrack.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../common/hooks';
 import { LoadContext, Track } from '../../common/types';
-import { addTrackToSelectedList, removeTrackFromSelectedList, trackIsOnSelectedList } from '../lists/listsSlice';
+import {
+  addTrackToActiveList,
+  removeTrackFromActiveList,
+  selectActiveList,
+  trackIsOnActiveList,
+} from '../lists/listsSlice';
 import { AddRemoveListButton } from './AddRemoveListButton';
 import { BaseTrack } from './BaseTrack';
 
 export function BrowserTrack({ id, context }: { id: Track['id']; context: LoadContext }) {
   const dispatch = useAppDispatch();
-  const isOnSelectedList = useAppSelector(trackIsOnSelectedList({ trackId: id }));
+  const hasActiveList = !!useAppSelector(selectActiveList);
+  const isOnSelectedList = useAppSelector(trackIsOnActiveList({ trackId: id }));
 
   return (
     <BaseTrack
       id={id}
       context={context}
       additionalButtons={
-        <AddRemoveListButton
-          isOnSelectedList={isOnSelectedList}
-          onClick={() => {
-            if (isOnSelectedList) {
-              dispatch(removeTrackFromSelectedList({ trackId: id }));
-            } else {
-              dispatch(addTrackToSelectedList({ trackId: id }));
-            }
-          }}
-        />
+        hasActiveList ? (
+          <AddRemoveListButton
+            isOnSelectedList={isOnSelectedList}
+            onClick={() => {
+              if (isOnSelectedList) {
+                dispatch(removeTrackFromActiveList({ trackId: id }));
+              } else {
+                dispatch(addTrackToActiveList({ trackId: id }));
+              }
+            }}
+          />
+        ) : (
+          <> </>
+        )
       }
     />
   );
